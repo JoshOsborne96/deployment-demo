eb3920b6129c63b8ac104234ce27c14ee3a9bd5d
Josh Osborne <Josh.Osborne@devmounta.in>
2022-09-13 00:07:32 -0600

    check 1

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get('/', function(req, res) {
+app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
