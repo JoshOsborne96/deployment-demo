83dec598a97470de27a9998c9b34c85609cd1c70
Josh Osborne <Josh.Osborne@devmounta.in>
2022-09-13 00:35:10 -0600

    add image folder

--- server/server.js
+++ server/server.js
@@ -6,6 +6,10 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../public'))
+})
+
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, '../public/index.html'))
 })
